0635e7dc0568ae62a67bc664e466fd8568ad6121
Ansh <2016pceecansh012@poornima.org>
2021-04-07 21:17:35 +0530

    First Commit

---
+++ Array_Merge/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int i = 0;
+        int j = 0;
+
+        int[] array1 = new int[6] { 33, 45, 67, 80, 4, 10000 };
+        int[] array2 = new int[6] { 3, 78, 49, 36, 1000, 98 };
+        int[] finalArray = new int[12];
+
+        for (i = 0, j = 0; i < 6; i++)
+        {
+            finalArray[j++] = array1[i];
+        }
+        for (i = 0; i < 6; i++)
+        {
+            finalArray[j++] = array2[i];
+        }
+
+        Array.Sort(finalArray);
+
+        foreach (int value in finalArray)
+        {
+            Console.Write(value + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ Enum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+    public class Program
+    {
+        public enum Days
+        {
+            Sunday, 
+            Monday, 
+            Tuesday, 
+            Wednesday, 
+            Thursday, 
+            Friday, 
+            Saturday
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Read values of the Color enum");
+            foreach (Days day in Enum.GetValues(typeof(Days)))
+                Console.WriteLine(day);
+        }
+    }
+
